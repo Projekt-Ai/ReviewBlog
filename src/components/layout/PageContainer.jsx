84ed19341d7bb7c908b2c9e2f5c20aac433cb2c9
Christian <101080884+Projekt-Ai@users.noreply.github.com>
2026-01-10 09:20:27 -0500

    Change main container class to 'rb-container'

--- src/components/layout/PageContainer.jsx
+++ src/components/layout/PageContainer.jsx
@@ -1,3 +1,3 @@
 export default function PageContainer({ children }) {
-  return <main className="mx-auto max-w-6xl p-4">{children}</main>;
+  return <main className="rb-container">{children}</main>;
 }
